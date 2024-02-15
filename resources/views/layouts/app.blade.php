3f357f22eb263ece802427c0d2d846f8b8a5e1eb
Anahel Delgado <anahelginesdelgadoalmeida@alumno.ieselrincon.es>
2024-02-15 12:15:44 +0000

    Maquetación de la vista general

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <title>Proyecto SmartEcoSchool</title>
+    </head>
+    <body>
+        <!-- header --> 
+        <header class="masthead bg-primary text-white text-center py-4">
+            <div class="container d-flex align-items-center flex-column">
+                <h1>@yield('title', 'SmartEcoSchool IES El Rincon')</h1>
+            </div>
+        </header>
+        <!-- header --> 
+
+        <!-- footer -->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container">
+            <small>
+                SmartEcoSchool Proyect - 2º DAW
+            </small>
+            </div>
+        </div>
+        <!-- footer -->
+    </body>
+</html>
